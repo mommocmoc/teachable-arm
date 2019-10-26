8d2f5e71b7c03e3ceff650a3c1445fea70178680
JaeHwan So <cowjh@nabi.or.kr>
2019-10-27 00:31:41 +0900

    Learning and Classifying Test Done in Desktop Browser

--- sketch.js
+++ sketch.js
@@ -1,12 +1,82 @@
-function setup(){
-    var canvas = createCanvas(320,240);
-    canvas.parent('video')
-    video = createCapture(VIDEO);
-    video.size(320,240);
-    video.hide();
+var logits = new Array(3);
+let buttons;
+let video;
+let features;
+let knn;
+let labelP;
 
+function setup() {
+  var canvas = createCanvas(320, 240);
+  //canvas.parent("videoDiv");
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.parent("videoDiv");
+  //video.hide();
+  canvas.hide();
+  console.log("ml5 version:", ml5.version);
+  features = ml5.featureExtractor("MobileNet", modelReady);
+  knn = ml5.KNNClassifier();
+  buttons = selectAll("button");
+  buttons[0].mousePressed(learningA);
+  buttons[1].mousePressed(learningB);
+  buttons[2].mousePressed(learningC);
+  let result = createButton("Result");
+  result.mousePressed(resultEvent);
+  labelP = createP("학습데이터가 필요합니다!")
 }
 
-function draw(){
-    image(video,0,0)
+//  features.infer(video);
+
+function modelReady() {
+  console.log("model ready!");
+}
+
+function learningA() {
+  const currentVideo = features.infer(video);
+  knn.addExample(currentVideo, "ClassA");
+  console.log("클래스 A 학습");
+}
+function learningB() {
+  const currentVideo = features.infer(video);
+  knn.addExample(currentVideo, "ClassB");
+  console.log("클래스 B 학습");
+}
+function learningC() {
+  const currentVideo = features.infer(video);
+  knn.addExample(currentVideo, "ClassC");
+  console.log("클래스 C 학습");
+}
+function gotResult(err, result) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(result);
+  }
+}
+function goClassify() {
+  const logits = features.infer(video);
+  
+    knn.classify(logits, function(err, result) {
+      if (err) {
+        console.error(err);
+      } else {
+        labelP.html(result.label + " " + result.confidencesByLabel[result.label]);
+        goClassify();
+      }
+    });
+}
+
+function resultEvent() {
+  const logits = features.infer(video);
+  if (knn.getNumLabels() > 0) {
+    knn.classify(logits, gotResult);
+  }
+  // console.log(test.dataSync());
+}
+
+function draw() {
+  image(video, 0, 0);
+  if (knn.getNumLabels() > 0) {
+  goClassify();
+  }
 }
